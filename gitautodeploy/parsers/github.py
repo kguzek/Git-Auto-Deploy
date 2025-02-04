b0fba6afcbd5994ee41e6214328de88045a8dfd4
Konrad (PC) <konrad@guzek.uk>
2025-02-04 22:51:25 +0100

    make token type string for pushes with secret

--- gitautodeploy/parsers/github.py
+++ gitautodeploy/parsers/github.py
@@ -1,27 +1,36 @@
+"""GitHub request parser module"""
+
+import hashlib
+import hmac
+import json
+
 from .base import WebhookRequestParserBase
 
 
 class GitHubRequestParser(WebhookRequestParserBase):
+    """Request parser for GitHub"""
 
     def get_matching_projects(self, request_headers, request_body, action):
-        import json
+        """Gets a list of projects that match the incoming GitHub web hook request"""
 
         data = json.loads(request_body)
 
         repo_urls = []
 
-        github_event = 'x-github-event' in request_headers and request_headers['x-github-event']
+        github_event = (
+            "x-github-event" in request_headers and request_headers["x-github-event"]
+        )
 
-        action.log_info("Received '%s' event from GitHub" % github_event)
+        action.log_info(f"Received '{github_event}' event from GitHub")
 
-        if 'repository' not in data:
+        if "repository" not in data:
             action.log_error("Unable to recognize data format")
             return []
 
         # One repository may posses multiple URLs for different protocols
-        for k in ['url', 'git_url', 'clone_url', 'ssh_url']:
-            if k in data['repository']:
-                repo_urls.append(data['repository'][k])
+        for k in ["url", "git_url", "clone_url", "ssh_url"]:
+            if k in data["repository"]:
+                repo_urls.append(data["repository"][k])
 
         # Get a list of configured repositories that matches the incoming web hook reqeust
         repo_configs = self.get_matching_repo_configs(repo_urls, action)
@@ -33,16 +42,22 @@ class GitHubRequestParser(WebhookRequestParserBase):
         for repo_config in repo_configs:
 
             # Validate secret token if present
-            if 'secret-token' in repo_config and 'x-hub-signature' in request_headers:
-                if not self.verify_signature(repo_config['secret-token'], request_body, request_headers['x-hub-signature']):
-                    action.log_info("Request signature does not match the 'secret-token' configured for repository %s." % repo_config['url'])
+            if "secret-token" in repo_config and "x-hub-signature" in request_headers:
+                if not self.verify_signature(
+                    repo_config["secret-token"],
+                    request_body,
+                    request_headers["x-hub-signature"],
+                ):
+                    action.log_info(
+                        f"Request signature does not match the 'secret-token' configured for repository {repo_config["url"]}."
+                    )
                     return False
 
         return True
 
     def verify_signature(self, token, body, signature):
-        import hashlib
-        import hmac
-
-        result = "sha1=" + hmac.new(str(token), body, hashlib.sha1).hexdigest()
+        """Verify the signature of the incoming request"""
+        result = (
+            "sha1=" + hmac.new(token.encode("utf-8"), body, hashlib.sha1).hexdigest()
+        )
         return result == signature
